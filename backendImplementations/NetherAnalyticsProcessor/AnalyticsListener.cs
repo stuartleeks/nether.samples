f806954f57d158356b988e4758b20bab193cb124
Andreas Pohl <Annonator@googlemail.com>
2017-05-18 20:16:24 +0200

    Added sample Nether integration into Service Fabric

---
+++ backendImplementations/NetherAnalyticsProcessor/AnalyticsListener.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Nether.Analytics;
+using Nether.Analytics.DataLake;
+using Nether.Analytics.EventHubs;
+using Nether.Analytics.Parsers;
+
+namespace NetherAnalyticsProcessor
+{
+    public class AnalyticsListener : ICommunicationListener
+    {
+        private readonly EventHubsListenerConfiguration configuration;
+        private readonly string dataLakeName;
+        private MessageProcessor<EventHubListenerMessage> messageProcessor;
+        private readonly ServiceClientCredentials serviceClientCredentials;
+        private readonly string subscription;
+
+        public AnalyticsListener(EventHubsListenerConfiguration configuration,
+            ServiceClientCredentials servicePrinciple, string subscription, string dataLakeName)
+        {
+            this.configuration = configuration;
+            this.serviceClientCredentials = servicePrinciple;
+            this.dataLakeName = dataLakeName;
+            this.subscription = subscription;
+        }
+
+        public async Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            var listener = new EventHubsListener(this.configuration);
+
+            var parser = new EventHubListenerMessageJsonParser();
+
+            var builder = new MessageRouterBuilder();
+
+            var clusteringSerializer = new CsvOutputFormatter("id", "type", "version", "enqueueTimeUtc",
+                "gameSessionId", "lat", "lon", "geoHash", "geoHashPrecision", "geoHashCenterLat", "geoHashCenterLon",
+                "rnd");
+
+            var clusteringDlsOutputManager = new DataLakeStoreOutputManager(
+                clusteringSerializer,
+                new PipelineDateFilePathAlgorithm(newFileOption: NewFileNameOptions.Every5Minutes),
+                this.serviceClientCredentials,
+                this.subscription,
+                this.dataLakeName);
+
+            var clusteringConsoleOutputManager = new ConsoleOutputManager(clusteringSerializer);
+
+            builder.Pipeline("clustering")
+                .HandlesMessageType("geo-location", "1.0.0")
+                .HandlesMessageType("geo-location", "1.0.1")
+                .AddHandler(new RandomIntMessageHandler())
+                .OutputTo(clusteringConsoleOutputManager, clusteringDlsOutputManager);
+
+
+            // Build all pipelines
+            var router = builder.Build();
+
+            // Attach the differeing parts of the message processor together
+            this.messageProcessor = new MessageProcessor<EventHubListenerMessage>(listener, parser, router);
+
+            // The following method will never exit
+            //await messageProcessor.ProcessAndBlockAsync();
+
+            return this.configuration.EventHubPath;
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                // Trace Error
+                ServiceEventSource.Current.Message(ex.Message);
+                throw;
+            }
+        }
+
+        public void Abort()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Trace Error
+                ServiceEventSource.Current.Message(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await this.messageProcessor.ProcessAndBlockAsync(cancellationToken);
+        }
+    }
+}
---
+++ backendImplementations/NetherAnalyticsProcessor/AnalyticsProcessor.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using Microsoft.Rest.Azure.Authentication;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Nether.Analytics.EventHubs;
+
+namespace NetherAnalyticsProcessor
+{
+    /// <summary>
+    ///     An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class AnalyticsProcessor : StatelessService
+    {
+        private const string ParameterCannotBeNullFormat =
+            "The parameter [{0}] is not defined in the Setting.xml configuration file.";
+
+        private AnalyticsListener _listener;
+
+        public AnalyticsProcessor(StatelessServiceContext context)
+            : base(context)
+        {
+        }
+
+        private EventHubsListenerConfiguration getEventHubsListenerConfiguration(ConfigurationPackage config)
+        {
+            if (!config.Settings.Sections.Contains("EventHubConfig"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "EventHubConfig"),
+                    "EventHubConfig");
+
+            var eventHubConfig = config.Settings.Sections["EventHubConfig"];
+
+            if (!eventHubConfig.Parameters.Contains("EventHubPath"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "EventHubPath"), "EventHubPath");
+
+
+            if (!eventHubConfig.Parameters.Contains("ConsumerGroupName"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "ConsumerGroupName"),
+                    "ConsumerGroupName");
+
+
+            if (!eventHubConfig.Parameters.Contains("EventHubConnectionString"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "EventHubConnectionString"),
+                    "EventHubConnectionString");
+
+
+            if (!eventHubConfig.Parameters.Contains("StorageConnectionString"))
+                throw new ArgumentException(
+                    string.Format(ParameterCannotBeNullFormat, "EventHuStorageConnectionStringbPath"),
+                    "StorageConnectionString");
+
+
+            if (!eventHubConfig.Parameters.Contains("LeaseContainerName"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "LeaseContainerName"),
+                    "LeaseContainerName");
+
+            return new EventHubsListenerConfiguration
+            {
+                ConsumerGroupName = eventHubConfig.Parameters["ConsumerGroupName"].Value,
+                EventHubConnectionString = eventHubConfig.Parameters["EventHubConnectionString"].Value,
+                EventHubPath = eventHubConfig.Parameters["EventHubPath"].Value,
+                LeaseContainerName = eventHubConfig.Parameters["LeaseContainerName"].Value,
+                StorageConnectionString = eventHubConfig.Parameters["StorageConnectionString"].Value
+            };
+        }
+
+        private async Task<ServiceClientCredentials> getServiceClientCredentials(ConfigurationPackage config)
+        {
+            if (!config.Settings.Sections.Contains("ServicePrinciple"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "ServicePrinciple"),
+                    "ServicePrinciple");
+
+            var servicePrinciple = config.Settings.Sections["ServicePrinciple"];
+
+            if (!servicePrinciple.Parameters.Contains("Tenant"))
+                throw new ArgumentException(
+                    string.Format(ParameterCannotBeNullFormat, "Tenant"),
+                    "Tenant");
+
+            if (!servicePrinciple.Parameters.Contains("ObjectId"))
+                throw new ArgumentException(
+                    string.Format(ParameterCannotBeNullFormat, "ObjectId"),
+                    "ObjectId");
+
+            if (!servicePrinciple.Parameters.Contains("Secret"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "Secret"),
+                    "Secret");
+
+            return await ApplicationTokenProvider.LoginSilentAsync(servicePrinciple.Parameters["Tenant"].Value,
+                new ClientCredential(servicePrinciple.Parameters["ObjectId"].Value,
+                    servicePrinciple.Parameters["Secret"].Value));
+        }
+
+        private string getDataLakeName(ConfigurationPackage config)
+        {
+            if (!config.Settings.Sections.Contains("DataLake"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "DataLake"), "DataLake");
+
+            var datalakeConfig = config.Settings.Sections["DataLake"];
+
+            if (!datalakeConfig.Parameters.Contains("DataLakeStoreName"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "DataLakeStoreName"),
+                    "DataLakeStoreName");
+
+            return datalakeConfig.Parameters["DataLakeStoreName"].Value;
+        }
+
+        private string getSubscriptionId(ConfigurationPackage config)
+        {
+            if (!config.Settings.Sections.Contains("DataLake"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "DataLake"), "DataLake");
+
+            var datalakeConfig = config.Settings.Sections["DataLake"];
+
+            if (!datalakeConfig.Parameters.Contains("SubscriptionId"))
+                throw new ArgumentException(string.Format(ParameterCannotBeNullFormat, "SubscriptionId"),
+                    "SubscriptionId");
+
+            return datalakeConfig.Parameters["SubscriptionId"].Value;
+        }
+
+
+        private ICommunicationListener createCommunicationListener()
+        {
+            var codePackageActivationContext = this.Context.CodePackageActivationContext;
+            var config = codePackageActivationContext.GetConfigurationPackageObject("Config");
+
+            this._listener = new AnalyticsListener(this.getEventHubsListenerConfiguration(config),
+                this.getServiceClientCredentials(config).Result, this.getSubscriptionId(config),
+                this.getDataLakeName(config));
+
+            return this._listener;
+        }
+
+        /// <summary>
+        ///     Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[]
+            {
+                new ServiceInstanceListener(context => this.createCommunicationListener(), "AnalyticsListener")
+            };
+        }
+
+        /// <summary>
+        ///     This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await this._listener.StartAsync(cancellationToken);
+        }
+    }
+}
